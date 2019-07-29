dcfe35613bf51527f14047ad317523e8c5491f91
chongjh <36836106+chongjh@users.noreply.github.com>
2019-07-29 23:29:42 +0800

    generated authentication items

---
+++ resources/views/login.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <form name="login-form" method="POST">
+                <div>
+                    <label for="email">Email </label>
+                    <input type="text" name="email" value="" />
+                </div>
+                <div>
+                    <label for="password">Password </label>
+                    <input type="password" name="password" value="" />
+                </div>
+                <input type="submit" value="Login" name="btn-register" />
+            </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
