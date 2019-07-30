35a1943c63846be76e43a70ab053a15d68022dd8
chongjh <36836106+chongjh@users.noreply.github.com>
2019-07-31 02:10:46 +0800

    .

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,26 +3,26 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Model\Faculty;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Validation\Rule;
 
-class RegisterController extends Controller
-{
+class RegisterController extends Controller {
     /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
+      |--------------------------------------------------------------------------
+      | Register Controller
+      |--------------------------------------------------------------------------
+      |
+      | This controller handles the registration of new users as well as their
+      | validation and creation. By default this controller uses a trait to
+      | provide this functionality without requiring any additional code.
+      |
+     */
 
-    use RegistersUsers;
+use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
@@ -36,8 +36,7 @@ class RegisterController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('guest');
     }
 
@@ -47,14 +46,36 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'string',  Rule::in(['Admin', 'Staff'])]
-        ]);
+    protected function validator(array $data) {
+        if ($data['role'] === 'Admin') {
+            return Validator::make($data, [
+                        'name' => ['required', 'string', 'regex:/^[A-z\(\)\-\@\ ]{1,255}$/'],
+                        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                        'password' => ['required', 'string', 'min:8', 'confirmed'],
+                        'role' => ['required', 'string', Rule::in(['Admin'])]
+            ]);
+        } else if ($data['role'] === 'Staff') {
+            if (Faculty::where('faculty_name', '=', $data['faculty'])->first()) {
+                return Validator::make($data, [
+                            'name' => ['required', 'string', 'regex:/^[A-z\(\)\-\@\ ]{1,255}$/'],
+                            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                            'password' => ['required', 'string', 'min:8', 'confirmed'],
+                            //problem
+                            'role' => ['required', 'string', Rule::in(['Staff'])],
+                            //problem
+                            'faculty' => ['required', 'string', 'regex:/^[A-z\(\)\-\@\, ]{2,255}$/'],
+                            'specialization' => ['nullable', 'string', 'regex:/^[A-z\(\)\-\@\, ]{0,255}$/'],
+                            'interest' => ['nullable', 'string', 'regex:/^[A-z\(\)\-\@\, ]{0,255}$/'],
+                            'position' => ['required', 'string', Rule::in(['Dean', 'Lecturer', 'Tutor'])]
+                ]);
+            } else {
+                //problem
+                return redirect()->back()->with('addStatus', false);
+            }
+        }
+
+
+        //mk validator for staff
     }
 
     /**
@@ -63,15 +84,14 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data)
-    {
+    protected function create(array $data) {
         $user = User::create([request(),
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'role' => $data['role'],
+                    'name' => $data['name'],
+                    'email' => $data['email'],
+                    'password' => Hash::make($data['password']),
+                    'role' => $data['role'],
         ]);
-        
+
 
 //        if($data) {
 //            new Admin
@@ -79,4 +99,5 @@ class RegisterController extends Controller
 //        }
         return $user;
     }
+
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,9 +18,9 @@
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +32,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -46,9 +46,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -60,41 +60,89 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-                        
+
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
 
                             <div class="col-md-6">
-                                <!--<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>-->
-
-                                <select name="role" class="form-control">
-                                    <option value="Admin">Admin</option>
-                                    <option value="Staff">Staff</option>
+                                <select onchange="showDiv(this)" name="role" class="form-control @error('role') is-invalid @enderror">
+                                    <option value="Admin" selected="selected"@if(old('role') === 'Admin'){{ 'selected="selected"' }}@endif>Admin</option>
+                                    <option value="Staff" @if(old('role') === 'Staff'){{ 'selected="selected"' }}@endif>Staff</option>
                                 </select>
-                                
-<!--                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror-->
+
+                                @error('role')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="Faculty" class="col-md-4 col-form-label text-md-right">{{ __('Faculty') }}</label>
+                        @php( $faculties = \App\Model\Faculty::all() )
+                        <div class="form-group row staff" style="display: none">
+                            <label for="faculty" class="col-md-4 col-form-label text-md-right">{{ __('Faculty') }}</label>
 
                             <div class="col-md-6">
-<!--                                <input id="faculty" type="text" class="form-control @error('faculty') is-invalid @enderror" name="faculty" required autocomplete="new-password">
+                                <select name="faculty" class="form-control @error('faculty') is-invalid @enderror">
+                                    @foreach ($faculties as $faculty)
+                                    <option  value="{{ $faculty->faculty_name }}" @if(old('faculty') === '{{ $faculty->faculty_name }}'){{ 'selected="selected"' }}@endif>{{ $faculty->faculty_name }}</option>
+                                    @endforeach
+                                </select>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror-->
-                                
+                                @error('faculty')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </div>
-                        
+
+                        <div class="form-group row staff" style="display: none">
+                            <label for="specialization" class="col-md-4 col-form-label text-md-right">{{ __('Specialization') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea id="specialization" rows="4" cols="20" class="form-control @error('specialization') is-invalid @enderror" name="specialization" autocomplete="specialization"></textarea>
+
+                                @error('specialization')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row staff" style="display: none">
+                            <label for="interest" class="col-md-4 col-form-label text-md-right">{{ __('Area(s) of Interest') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea id="interest" rows="4" cols="20" class="form-control @error('interest') is-invalid @enderror" name="interest" autocomplete="interest"></textarea>
+
+                                @error('interest')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row staff" style="display: none">
+                            <label for="position" class=" col-md-4 col-form-label text-md-right">{{ __('Position') }}</label>
+
+                            <div class="col-md-6">
+                                <select name="position" class="form-control @error('position') is-invalid @enderror">
+                                    <option value="Lecturer" @if(old('position') === 'Lecturer'){{ 'selected="selected"' }}@endif>Lecturer</option>
+                                    <option value="Tutor" @if(old('position') === 'Tutor'){{ 'selected="selected"' }}@endif>Tutor</option>
+                                    <option value="Dean" @if(old('position') === 'Dean'){{ 'selected="selected"' }}@endif>Dean</option>
+                                </select>
+
+                                @error('position')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -108,4 +156,14 @@
         </div>
     </div>
 </div>
+
+<script>
+    function showDiv(element)
+    {
+        Array.from(document.querySelectorAll('.staff')).forEach((item) => {
+          item.style.display = element.value == 'Staff' ? '' : 'none'; 
+        })
+    }
+</script>
+
 @endsection
