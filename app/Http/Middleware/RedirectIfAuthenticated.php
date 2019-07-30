2c154a2bb3c931c639e64160701e4284f38381d2
chongjh <36836106+chongjh@users.noreply.github.com>
2019-07-30 21:03:00 +0800

    registered redirected to admin/staff home

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
-{
+class RedirectIfAuthenticated {
+
     /**
      * Handle an incoming request.
      *
@@ -15,12 +15,15 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
-    {
+    public function handle($request, Closure $next, $guard = null) {
         if (Auth::guard($guard)->check()) {
+            if (Auth::check() && Auth::user()->role === 'Admin') {
+                return redirect('/admin/home');
+            }
             return redirect('/home');
         }
 
         return $next($request);
     }
+
 }
