5a36943bb457dd042b0b775dcf8ce3c9a8c5a9e2
chongjh <36836106+chongjh@users.noreply.github.com>
2019-08-05 00:57:24 +0800

    added xml generation, dtd

--- app/Http/Controllers/StaffControllers/StaffController.php
+++ app/Http/Controllers/StaffControllers/StaffController.php
@@ -30,6 +30,7 @@ class StaffController extends Controller {
         // Create XSLT Processor
         $proc = new XSLTProcessor();
         $proc->importStyleSheet($xsl);
+        $this->saveXML($staffList->collection);
 
         $staffList = $proc->transformToXML($xml);
 
@@ -49,6 +50,46 @@ class StaffController extends Controller {
             }
         }
         return $xml->asXML();
+    }
+
+    public function saveXML($staffListArray) {
+        //Creates XML string and XML document using the DOM 
+        $dom = new DomDocument('1.0', 'UTF-8');
+
+        $implementation = new \DOMImplementation();
+        $dom->appendChild($implementation->createDocumentType('staffList SYSTEM \'staff.dtd\''));
+        
+        //add root
+        $staffList = $dom->appendChild($dom->createElement('staffList'));
+
+        for ($i = 0; $i < count($staffListArray); $i++) {
+
+            //add track element to jukebox
+            $staff = $dom->createElement('staff');
+            $staffList->appendChild($staff);
+
+            // Appending attributes
+            $element = $dom->createElement('name');
+            $element->appendChild($dom->createTextNode($staffListArray[$i]['name']));
+            $staff->appendChild($element);
+            $element = $dom->createElement('position');
+            $element->appendChild($dom->createTextNode($staffListArray[$i]['position']));
+            $staff->appendChild($element);
+            $element = $dom->createElement('faculty');
+            $element->appendChild($dom->createTextNode($staffListArray[$i]['faculty_id']));
+            $staff->appendChild($element);
+            $element = $dom->createElement('specialization');
+            $element->appendChild($dom->createTextNode($staffListArray[$i]['specialization']));
+            $staff->appendChild($element);
+            $element = $dom->createElement('area_of_interest');
+            $element->appendChild($dom->createTextNode($staffListArray[$i]['area_of_interest']));
+            $staff->appendChild($element);
+        }
+
+        $dom->formatOutput = true; // set the formatOutput attribute of domDocument to true
+        // save XML as string or file 
+        $staffListXML = $dom->saveXML(); // put string in test1
+        $dom->save('../storage/app/public/xml/staff/staff_list.xml'); // save as file
     }
 
     public function getStaffInfo() {
