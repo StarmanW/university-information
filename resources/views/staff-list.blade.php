c9fcf3f0cac739213e5f2167e39e860aee48c6fb
chongjh <36836106+chongjh@users.noreply.github.com>
2019-08-05 01:28:06 +0800

    changed styling

--- resources/views/staff-list.blade.php
+++ resources/views/staff-list.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 mx-auto">
-            <table class="table table-responsive sortable">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Faculty</th>
-                        <th>Specialization</th>
-                        <th>Area of Interest</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {!!$staffList!!}
-                </tbody>
-            </table>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"></div>
+
+                <div class="card-body mx-auto">
+                    <table border="0" class="table table-responsive table-striped">
+                        <thead class="thead-dark">
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Faculty</th>
+                            <th>Specialization</th>
+                            <th>Area of Interest</th>
+                        </thead>
+                        <tbody>
+                            {!!$staffList!!}
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
