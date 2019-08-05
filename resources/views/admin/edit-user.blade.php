f20a00beb9c0c7d4c651721e17b079d4fb3b704c
chongjh <36836106+chongjh@users.noreply.github.com>
2019-08-05 15:17:32 +0800

    added error prompt

--- resources/views/admin/edit-user.blade.php
+++ resources/views/admin/edit-user.blade.php
@@ -3,6 +3,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            
+            @if (session('roleError') !== null)
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <strong>{{ session('roleError') }}</strong>
+            </div>
+            @endif
+            
             <div class="card">
                 <div class="card-header"></div>
 
